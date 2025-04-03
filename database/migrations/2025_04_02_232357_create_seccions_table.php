8552856b87e07c38b554dbe95293f02220daec12
Rafael Castillo <RafaelcastilloStark2001@gmail.com>
2025-04-02 19:20:56 -0600

    Se corrigieron errores en tablas de base de datos

--- database/migrations/2025_04_02_232357_create_seccions_table.php
+++ database/migrations/2025_04_02_232357_create_seccions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('seccions', function (Blueprint $table) {
+        Schema::create('secciones', function (Blueprint $table) {
             $table->id();
             $table->foreignId('docente_id')->constrained();
             $table->string('nombre');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('seccions');
+        Schema::dropIfExists('secciones');
     }
 };
